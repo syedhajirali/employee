16154789c38a07247dc72f68bd539178a1ce133f
M Syed Hajir Ali <syedhajirali.m@centrico.tech>
2020-09-08 17:16:21 +0530

    Initial changes

---
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -0,0 +1,16 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+	@GetMapping("/test")
+	public String sayTest() {
+		return "Welcome to Kubernetes Jenkins Demo-xxx Changes";
+	}
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "sayHello syed--TTTTTTT";
+	}
+}
