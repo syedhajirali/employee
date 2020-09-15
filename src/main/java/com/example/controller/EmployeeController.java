bd4265fca1c8720823b6b1531e24614106b8d08a
M Syed Hajir Ali <syedhajirali.m@centrico.tech>
2020-09-15 12:59:19 +0530

    changes

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -1,10 +1,19 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.Employee;
+import com.example.service.EmployeeService;
 
 @RestController
 public class EmployeeController {
+	@Autowired
+	EmployeeService empService;
 	@GetMapping("/test")
 	public String sayTest() {
 		return "Welcome to Kubernetes Jenkins Demo-xxx Changes";
@@ -18,5 +27,16 @@ public class EmployeeController {
 	public String getAllStudents() {
 		return "all Employees";
 
+	}
+	@GetMapping("/employees")
+	public List<Employee> getEmployees() {
+
+		return empService.getAllEmployees();
+
+	}
+
+	@GetMapping("/insertemployee")
+	public void insertEmployee(@RequestBody Employee employee) {
+		empService.insertEmployee(employee);
 	}
 }
---
+++ src/main/java/com/example/service/EmployeeService.java
@@ -0,0 +1,10 @@
+package com.example.service;
+
+import java.util.List;
+
+import com.example.model.Employee;
+
+public interface EmployeeService {
+	List<Employee> getAllEmployees();
+	void insertEmployee(Employee employee);
+}
